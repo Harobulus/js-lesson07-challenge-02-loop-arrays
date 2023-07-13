2b521dccfdfdb3d0d424fb6a51166ef53add5c1c
Harobulus <52546682+Harobulus@users.noreply.github.com>
2023-07-13 20:06:22 +0100

    Practiced for...of, foreach and filter

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ var programmingLanguages = [
   "Rust",
   "C#"
 ];
+
+console.log(programmingLanguages);
+
+// Output Strings with a for...of Loop
+for (var program of programmingLanguages) {
+  console.log(`I want to learn ${program}!`);
+}
+
+// Create a NUmbered List with forEach()
+programmingLanguages.forEach(function (langX, indexX) {
+  console.log(`${indexX + 1}. ${langX}`.toUpperCase());
+});
+
+// Filter Elements by a String
+var updatedLanguages = programmingLanguages.filter(function (language) {
+  return language.includes("y");
+});
+
+console.log(updatedLanguages);
